6f0346892a3a0988c6621098a7eac047da294ffa
Wycliffe <wycliffekiprono4@gmail.com>
2023-04-22 11:18:15 +0300

    used array .map, .filter, ...spread

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,11 +6,33 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood]
+    setFoods(newFoodArray)
+  }
+
+  function handleClick (id) {
+    const newFoodArray = foods.filter((food) => food.id !== id)
+    setFoods(newFoodArray)
+  }
+  
+  function handleLiClick(id) {
+    // .map will iterate through the array and return a new array
+    // if the id of the food we are iterating over matches the id of the food we are updating, return a new food object with the healt leavel incremented by 1
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+         return {
+          ...food, 
+          heatLevel: food.heatLevel + 1
+         }
+      } else {
+          return food
+       } 
+    })
+    setFoods(newFoodArray)
   }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
